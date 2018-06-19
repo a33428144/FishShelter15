c9dd032c157b95c2410075d9558734f718a2ff40
felixyu10 <felixyu@accuvally.com>
2018-06-19 23:22:35 +0800

    修改footer

--- FishShelter/App_Start/BundleConfig.cs
+++ FishShelter/App_Start/BundleConfig.cs
@@ -8,24 +8,21 @@ namespace FishShelter
         // 如需「搭配」的詳細資訊，請瀏覽 http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
-            // 準備好實際執行時，請使用 http://modernizr.com 上的建置工具，只選擇您需要的測試。
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            //強制啟用打包及最佳化
+            BundleTable.EnableOptimizations = true;
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/js/jquery.js",
+                        "~/js/jquery-ui.min.js",
+                        "~/js/bootstrap.min.js",
+                        "~/js/ma5slider.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/css/bootstrap.min.css",
+                      "~/css/style.css",
+                      "~/font-awesome/css/font-awesome.min.css",
+                      
+                      "~/css/ma5slider.css"));
+
         }
     }
 }
