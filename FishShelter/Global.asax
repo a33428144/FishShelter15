665647555f1a57ffeb53d2514f935f43e78d5230
felixyu10 <felixyu@accuvally.com>
2018-06-19 21:29:20 +0800

    first commit

---
+++ FishShelter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FishShelter.MvcApplication" Language="C#" %>
---
+++ FishShelter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FishShelter.Startup))]
+namespace FishShelter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
