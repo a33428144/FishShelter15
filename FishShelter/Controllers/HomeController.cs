5aeaab6a787a41026c1cc8955325a94676e7a0cd
a33428144 <a33428144@hotmail.com>
2020-05-30 16:32:20 +0800

    no message

--- FishShelter/Controllers/HomeController.cs
+++ FishShelter/Controllers/HomeController.cs
@@ -45,37 +45,10 @@ namespace FishShelter.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Contact(string a)
+        [HttpGet]
+        public ActionResult TV()
         {
-            try
-            {
-                MailMessage msg = new MailMessage();
-                msg.To.Add("a33428144@hotmail.com");
-                msg.From = new MailAddress("a33428144@gmail.com", "Felix", System.Text.Encoding.UTF8);
-                //郵件標題 
-                msg.Subject = "魚礁十五號網站留言";
-                //郵件標題編碼  
-                msg.SubjectEncoding = System.Text.Encoding.UTF8;
-                //郵件內容
-                msg.Body = "嘿嘿嘿";
-                msg.IsBodyHtml = true;
-                msg.BodyEncoding = System.Text.Encoding.UTF8;//郵件內容編碼 
-                msg.Priority = MailPriority.Normal;//郵件優先級 
-                                                   //建立 SmtpClient 物件 並設定 Gmail的smtp主機及Port 
-
-                SmtpClient smtep = new SmtpClient("smtp.gmail.com", 587);
-                //設定你的帳號密碼
-                smtep.Credentials = new System.Net.NetworkCredential("a33428144@gmail.com", "Aa84586138");
-                //Gmial 的 smtp 使用 SSL
-                smtep.EnableSsl = true;
-                smtep.Send(msg);
-                return null;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            return View();
         }
     }
 }
